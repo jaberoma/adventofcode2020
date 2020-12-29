bc9638f72cde2beb218e9dc4f71d9de9a280fc94
Javi <jbr.swork@gmail.com>
2020-12-29 22:18:14 +0100

    Working solution. Solved, target value is 996075

---
+++ src/main/java/es/jaberoma/aoc/day1/MatchingNumbers.java
@@ -0,0 +1,21 @@
+package es.jaberoma.aoc.day1;
+
+public class MatchingNumbers {
+
+    int num1;
+    int num2;
+
+    public MatchingNumbers(int num1, int num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+}
